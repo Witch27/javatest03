e4bbee832f2fc2b63c9f2d2ff69b23f8cb5c51b1
Witch27 <109660713+Witch27@users.noreply.github.com>
2022-07-25 11:09:15 +0200

    javatest03
    javatest03

---
+++ src/main/java/com/example/demo6/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.demo6;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class HelloController {
+
+    @FXML
+    private TextField tfTitle;
+
+    @FXML
+    void btnOKClicked(ActionEvent event) {
+        Stage mainWindow = (Stage) tfTitle.getScene().getWindow();
+        String title = tfTitle.getText();
+        mainWindow.setTitle(title);
+
+    }
+
+}
